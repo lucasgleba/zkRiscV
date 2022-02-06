6b1c5018e8db25b0b98801504d33bcebd57d98c6
Lucas Gleba <lucasglebapuig@gmail.com>
2022-02-06 18:28:22 +0100

    Test for k=0 state

--- circuits/test/state.test.js
+++ circuits/test/state.test.js
@@ -26,15 +26,21 @@ describe("state", function () {
       for (let ii = 0; ii < mSize; ii++) {
         m[ii] = ii + 1;
       }
-      for (let ii = 0; ii < mSize; ii++) {
-        const w = await circuit.calculateWitness(
-          { in_dec: input, pointer_dec: ii, k: 1, mIn: m },
-          true
-        );
-        const temp = m[ii];
-        m[ii] = input;
-        await circuit.assertOut(w, { mOut: m });
-        m[ii] = temp;
+      for (let kk = 0; kk < 2; kk++) {
+        for (let ii = 0; ii < mSize; ii++) {
+          const w = await circuit.calculateWitness(
+            { in_dec: input, pointer_dec: ii, k: kk, mIn: m },
+            true
+          );
+          const temp = m[ii];
+          if (kk == 1) {
+            m[ii] = input;
+          }
+          await circuit.assertOut(w, { mOut: m });
+          if (kk == 1) {
+            m[ii] = temp;
+          }
+        }
       }
     });
   });
@@ -68,24 +74,26 @@ describe("state", function () {
       for (let ii = 0; ii < nRegisters; ii++) {
         r[ii] = ii + 1;
       }
-      for (let ii = 0; ii < nRegisters + 1; ii++) {
-        const address_bin = ii
-          .toString(2)
-          .padStart(addressSize, "0")
-          .split("")
-          .reverse();
-        const w = await circuit.calculateWitness(
-          { in_dec: input, rIn: r, address_bin: address_bin, k: 1 },
-          true
-        );
-        let temp;
-        if (ii > 0) {
-          temp = r[ii - 1];
-          r[ii - 1] = input;
-        }
-        await circuit.assertOut(w, { rOut: r });
-        if (ii > 0) {
-          r[ii - 1] = temp;
+      for (let kk = 0; kk < 2; kk++) {
+        for (let ii = 0; ii < nRegisters + 1; ii++) {
+          const address_bin = ii
+            .toString(2)
+            .padStart(addressSize, "0")
+            .split("")
+            .reverse();
+          const w = await circuit.calculateWitness(
+            { in_dec: input, rIn: r, address_bin: address_bin, k: kk },
+            true
+          );
+          let temp;
+          if (ii > 0 && kk == 1) {
+            temp = r[ii - 1];
+            r[ii - 1] = input;
+          }
+          await circuit.assertOut(w, { rOut: r });
+          if (ii > 0 && kk == 1) {
+            r[ii - 1] = temp;
+          }
         }
       }
     });
